d5dd4363b7ea4aeb4f5aef2695cfc045598bd9e3
Camille <camille.meunier21@gmail.com>
2024-10-06 17:11:18 +0200

    Change click on wish to be on the whole ListGroupItem

--- src/components/WishCardItem.tsx
+++ src/components/WishCardItem.tsx
@@ -1,8 +1,8 @@
 import {Wish} from "../interfaces/WishListData";
-import {Badge, Card, ListGroupItem, OverlayTrigger, Stack, Tooltip} from "react-bootstrap";
-import {Link, useParams} from "react-router-dom";
+import {Badge, Button, Card, ListGroupItem, OverlayTrigger, Stack, Tooltip} from "react-bootstrap";
+import {useParams} from "react-router-dom";
 import {ArrowUpRightCircle} from "react-bootstrap-icons";
-import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
 import {WebSocketSendMessage} from "../interfaces/Websocket";
 import {useTranslation} from "react-i18next";
 
@@ -29,7 +29,7 @@ export default function WishCardItem(
         : Readonly<WishCardItemProps>) {
     const {t} = useTranslation();
     const {userToken} = useParams();
-    const [canShowAssignedUser, setCanShowAssignedUser]= useState<boolean>(!!wish.assignedUser && (!isCurrentUser || surpriseMode))
+    const [canShowAssignedUser, setCanShowAssignedUser] = useState<boolean>(!!wish.assignedUser && (!isCurrentUser || !surpriseMode))
 
     /**
      * Handle the assigned user of a wish, send the data to the api and refresh the wishlist data
@@ -60,16 +60,27 @@ export default function WishCardItem(
      * else assign the wish to the user
      * @param wish
      */
-    const HandleOnClick = (wish: Wish) => {
+    const HandleClickOnWish = (wish: Wish) => {
         // If the user is the current one,
         // then set the editWish state instead of assigning the wish
         if (isCurrentUser) {
             setEditWish(wish)
             setShowWishForm(true)
-        // Only the currently assigned user can modify the wish assignedUser
+            // Only the currently assigned user can modify the wish assignedUser
         } else if (!wish.assignedUser || wish.assignedUser === currentUserName) {
             handleAssignedUser(wish.id, !!wish.assignedUser)
         }
+    }
+
+    /**
+     * Handle click on url which is inside the element containing HandleClickOnWish
+     * We need to prevent it from being triggered and only open the link in another tab
+     * @param e
+     * @param wish
+     */
+    const HandleClickOnUrl = (e: React.MouseEvent<HTMLButtonElement>, wish: Wish) => {
+        e.stopPropagation();  // avoid triggering HandleClickOnWish on parent element
+        wish.url ? window.open(wish.url, "_blank") : null
     }
 
     /**
@@ -93,44 +104,47 @@ export default function WishCardItem(
     }, [wish, isCurrentUser, surpriseMode])
 
     return (
-         // If the wish is deleted and the current user is not the one who took it, we don't display it
+        // If the wish is deleted and the current user is not the one who took it, we don't display it
         // We continue to display it to the user who took it, to allow him to unassign it
         wish.deleted && wish.assignedUser !== currentUserName
-        ? null
-        :
-        <ListGroupItem key={wish.id}>
+            ? null
+            :
+            <ListGroupItem key={wish.id} onClick={() => HandleClickOnWish(wish)} className={"wish-group-item"}>
 
-            <Card.Title className={canShowAssignedUser ? "crossed-text" : ""}>
-                <Stack direction={"horizontal"} gap={3} >
-                    {/* double HandleOnClick to avoid triggering it on Link*/}
-                    <OverlayTrigger overlay={<Tooltip id="tooltip-take-wish">{TooltipContent(wish)}</Tooltip>}>
-                        <span onClick={() => HandleOnClick(wish)}>{wish.deleted ? t("wishCard.deleted") : wish.name }</span>
-                    </OverlayTrigger>
-                    <Badge className="ms-auto" onClick={() => HandleOnClick(wish)}>{wish.price}</Badge>
-                     {wish.url
-                        ? <OverlayTrigger overlay={<Tooltip id="tooltip-take-wish">{t("wishCard.tooltip.goToWebsite")}</Tooltip>}>
-                            <Link to={wish.url} target="_blank">
-                                <ArrowUpRightCircle className={"icon-link"}></ArrowUpRightCircle>
-                            </Link>
+                <Card.Title className={canShowAssignedUser ? "crossed-text" : ""}>
+                    <Stack direction={"horizontal"} gap={3}>
+                        <OverlayTrigger overlay={<Tooltip id="tooltip-take-wish">{TooltipContent(wish)}</Tooltip>}>
+                            <span>{wish.deleted ? t("wishCard.deleted") : wish.name}</span>
                         </OverlayTrigger>
-                        : null
-                    }
-                </Stack>
-            </Card.Title>
 
-            {/*Display taken by when surpriseMode is off*/}
-            {canShowAssignedUser?
-                <Card.Subtitle>
-                    <small className="text-muted">{t("wishCard.taken")}{wish.assignedUser}</small>
-                </Card.Subtitle>
-                : null
-            }
+                        <Badge className="ms-auto">{wish.price}</Badge>
 
-            {/*Display the description of the wish*/}
-            {wish.description
-                ? <Card.Text className="mt-2">{wish.description}</Card.Text>
-                : null
-            }
-        </ListGroupItem>
+                        {wish.url
+                            ? <OverlayTrigger overlay={
+                                <Tooltip id="tooltip-take-wish">{t("wishCard.tooltip.goToWebsite")}</Tooltip>
+                            }>
+                                <Button variant="link" className={"p-0"} onClick={(e) => HandleClickOnUrl(e, wish)}>
+                                    <ArrowUpRightCircle className={"icon-link"}></ArrowUpRightCircle>
+                                </Button>
+                            </OverlayTrigger>
+                            : null
+                        }
+                    </Stack>
+                </Card.Title>
+
+                {/*Display taken by when surpriseMode is off*/}
+                {canShowAssignedUser ?
+                    <Card.Subtitle>
+                        <small className="text-muted">{t("wishCard.taken")}{wish.assignedUser}</small>
+                    </Card.Subtitle>
+                    : null
+                }
+
+                {/*Display the description of the wish*/}
+                {wish.description
+                    ? <Card.Text className="mt-2">{wish.description}</Card.Text>
+                    : null
+                }
+            </ListGroupItem>
     )
 }
